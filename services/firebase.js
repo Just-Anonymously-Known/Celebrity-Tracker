d0c28e4ff2c016d1f75517fb61cea7386cfb2dee
Godis Akachukwu <akachukwugodis55@gmail.com>
2026-09-18 15:12:42 +0100

    added a libe of code to firebase.js

--- services/firebase.js
+++ services/firebase.js
@@ -1,6 +1,14 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
-const serviceAccount = require('../firebase-service-key.json');
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  // Parses the JSON string stored in Railway's environment variables
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  // Uses your local file when running on your computer
+  serviceAccount = require('../firebase-service-key.json');
+}
 
 // Initialize Firebase using the explicit SDK modular functions
 initializeApp({
